8d1e6df40d1b8bb61bf22c7785d72967de15477a
KashyapHegdeKota <kota2022kashyap@gmail.com>
2025-12-02 14:15:07 -0700

    Added routing for resume.

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/Resume',
+                destination: '/resume.pdf',
+                permanent: true,
+            }
+        ];
+    },
+};
 
 export default nextConfig;
